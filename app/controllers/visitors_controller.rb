f1fb0ce6200128edd48fcf2487ffb33758a27765
vagrant <vagrant@precise32.(none)>
2014-06-05 05:59:20 +0000

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,7 @@
 class VisitorsController < ApplicationController
 	def new
 		@owner = Owner.new
-		render 'visitors/new'
+		flash.now[:notice] = 'Welcome!'
+		flash.now[:alter] = 'My birthday is soon.'
 	end
 end
